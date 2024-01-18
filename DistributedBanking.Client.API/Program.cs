17bf5a09c31609e672f51d46468d05f5de2e4779
vadym.mulish <lamborghini43346@gmail.com>
2024-01-18 22:17:16 +0200

    migrate first part of the core client app functions

---
+++ DistributedBanking.Client.API/Program.cs
@@ -0,0 +1,23 @@
+using DistributedBanking.API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services
+    .AddApi(configuration)
+    .AddSwagger()
+    .AddBackgroundServices(configuration)
+    .AddServices(configuration)
+    .ConfigureOptions(configuration);
+
+builder.Host.UseSerilogAppLogging();
+
+var application = builder.Build();
+application
+    .UseAppSerilog()
+    .UseMiddleware()
+    .UseAutoWrapper()
+    .UseAppCore()
+    .UseAppSwagger();
+
+await application.RunAsync();
