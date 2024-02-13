6993d6ffa05e54a044cc3bc91a6a4f00f091eb77
vadym.mulish <lamborghini43346@gmail.com>
2024-02-13 23:31:21 +0200

    add role creation logic

--- DistributedBanking.Client.Domain/Services/IRolesManager.cs
+++ DistributedBanking.Client.Domain/Services/IRolesManager.cs
@@ -1,10 +1,6 @@
-﻿using Contracts.Models;
-using Shared.Data.Entities.Identity;
-
-namespace DistributedBanking.Client.Domain.Services;
+﻿namespace DistributedBanking.Client.Domain.Services;
 
 public interface IRolesManager
 {
-    Task<OperationResult> CreateAsync(ApplicationRole role);
     Task<bool> RoleExists(string roleName);
 }
--- DistributedBanking.Client.Domain/Services/Implementation/RolesManager.cs
+++ DistributedBanking.Client.Domain/Services/Implementation/RolesManager.cs
@@ -1,39 +1,17 @@
 ﻿using Contracts.Extensions;
-using Contracts.Models;
 using DistributedBanking.Client.Data.Repositories;
-using Microsoft.Extensions.Logging;
-using Shared.Data.Entities.Identity;
 
 namespace DistributedBanking.Client.Domain.Services.Implementation;
 
 public class RolesManager : IRolesManager
 {
     private readonly IRolesRepository _rolesRepository;
-    private readonly ILogger<RolesManager> _logger;
 
-    public RolesManager(
-        IRolesRepository rolesRepository,
-        ILogger<RolesManager> logger)
+    public RolesManager(IRolesRepository rolesRepository)
     {
         _rolesRepository = rolesRepository;
-        _logger = logger;
     }
     
-    public async Task<OperationResult> CreateAsync(ApplicationRole role)
-    {
-        try
-        {
-            await _rolesRepository.AddAsync(role);
-            return OperationResult.Success();
-        }
-        catch (Exception exception)
-        {
-            _logger.LogError(exception, "Exception occurred while trying to create a new role");
-            
-            return OperationResult.InternalFail("Error occurred while trying to create a new role");
-        }
-    }
-
     public async Task<bool> RoleExists(string roleName)
     {
         return (await _rolesRepository.GetAsync(r => r.NormalizedName == roleName.NormalizeString())).Any();
