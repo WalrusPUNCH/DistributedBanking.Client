9c05dc175f85ae0a1978bcf496107451301d92ce
vadym.mulish <lamborghini43346@gmail.com>
2024-02-16 01:25:00 +0200

    add ResponseService

---
+++ DistributedBanking.Client.Domain/Services/IResponseService.cs
@@ -0,0 +1,9 @@
+﻿using Confluent.Kafka;
+using Shared.Messaging.Messages;
+
+namespace DistributedBanking.Client.Domain.Services;
+
+public interface IResponseService
+{
+    Task<T?> GetResponse<T>(MessageBase messageBase, TopicPartitionOffset messageOffset, TimeSpan? timeout = null);
+}
